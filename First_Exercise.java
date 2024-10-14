32e8808b6dc32dc57e5a73fcbf08067dde2dff63
AbdurRehman013 <rehman.abdur200518@gmail.com>
2024-10-14 21:30:28 +0500

    Add files via upload

---
+++ First_Exercise.java
@@ -0,0 +1,32 @@
+
+
+package lab.first_exercise;
+import java.util.Scanner;
+
+public class First_Exercise {
+
+   
+
+  
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       
+        System.out.println("Enter your name : ");
+        String name=sc.nextLine();
+           System.out.println("Enter your enrollment : ");
+        String enrollment=sc.nextLine();
+         System.out.println("Enter your instructor name : ");
+        String instname=sc.nextLine();
+         System.out.println("Enter your subject: ");
+         String subject=sc.nextLine();
+          System.out.println("Enter your semester : ");
+        String semester=sc.nextLine();
+         System.out.println(" Name : "+name);
+         System.out.println(" Enrollment : "+enrollment);
+         System.out.println(" Subject : "+subject);
+         System.out.println(" Instructor Name : "+instname);
+         System.out.println(" Semester : "+semester);
+    }
+
+    }
+
